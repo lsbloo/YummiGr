cdc8a7e7437ff2c031b12c466f6d016144e3cbcc
Osvaldo Airon <osvaldo_cavalcanti_filho@hotmail.com>
2019-06-12 18:52:43 -0300

    adding_packet

---
+++ YummiGR/src/main/java/com/com/yummigr/graphics/GeneratorFreeChart.java
@@ -0,0 +1,11 @@
+package com.com.yummigr.graphics;
+
+/**
+ * @author osvaldoairon
+ *
+ *
+all the interactions of the messenger connector and creation of graphs
+specialized in the information of sending of sms and emails.
+ */
+public class GeneratorFreeChart {
+}
