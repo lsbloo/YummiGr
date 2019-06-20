d901138ba6e19322cadc475be1689c8b3f5d83a6
OsvaldoAiron <osvaldo_cavalcanti_filho@hotmail.com>
2019-06-20 01:48:53 -0300

    new_pack_archives

---
+++ YummiGR/src/main/java/com/com/yummigr/archives/ManagerFiles.java
@@ -0,0 +1,18 @@
+package com.com.yummigr.archives;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * save archives from graphs messenger_connector;(user);
+ * @author osvaldoairon
+ *
+ */
+@Component
+public class ManagerFiles implements Serializable{
+	
+	
+	
+
+}
